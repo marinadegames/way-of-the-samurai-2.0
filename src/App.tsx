dd550aa39f8517bfc0243a5394300a6b55f16b66
Marinade <marinadegames@gmail.com>
2021-12-02 21:30:06 +0300

    add props

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import './App.css';
 
 
-let starImg = <img src="https://img.icons8.com/ios/30/000000/star--v1.png"/>
+let starImg = <img src="https://img.icons8.com/ios/30/000000/star--v1.png" alt='png'/>
 
-function App() {
+function App(props:any) {
     return (
         <div className="App">
             <div className='Header'> Header </div>
@@ -16,17 +16,20 @@ function App() {
             <ButtonYellow />
             <ButtonBlue />
             <ButtonGreen />
+
+            <Accordion/>
+
         </div>
     );
 }
 
-let ButtonRed = () => <button className='ButtonRed'> red <b>alert</b>  </button>
-let ButtonYellow = () => <button className='ButtonYellow'> yellow <b>favorites</b>  </button>
-let ButtonBlue = () => <button className='ButtonBlue'> blue <b>info</b> </button>
-let ButtonGreen = () => <button className='ButtonGreen'> green <b>send</b> </button>
-let Star = () => <button className='ButtonStar'> {starImg} </button>
+let ButtonRed = (props:any) => <button className='ButtonRed'> red <b>alert</b>  </button>
+let ButtonYellow = (props:any) => <button className='ButtonYellow'> yellow <b>favorites</b>  </button>
+let ButtonBlue = (props:any) => <button className='ButtonBlue'> blue <b>info</b> </button>
+let ButtonGreen = (props:any) => <button className='ButtonGreen'> green <b>send</b> </button>
+let Star = (props:any) => <button className='ButtonStar'> {starImg} </button>
 
-function Rating() {
+function Rating(props:any) {
     return (
         <div className='Rating'>
             <h3>Rating:</h3>
@@ -39,5 +42,25 @@ function Rating() {
     )
 }
 
+function Accordion(props:any){
+    return(
+        <div className='Accordion'>
+            <AccordionTitle/>
+            <AccordionBody/>
+        </div>
+    )
+}
+function AccordionTitle(props:any){
+    return <h3>Меню:</h3>
+}
+function AccordionBody(props:any){
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
 
 export default App;
