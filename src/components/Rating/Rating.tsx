ae6227739d47fd2afd1c004bb965d2e16a16528c
Marinade <marinadegames@gmail.com>
2021-12-21 20:37:17 +0300

    code review <Star/>

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -9,9 +9,9 @@ type RatingProps = {
     //value: number,
 }
 type StarPropsType = {
-    id: number
     selected: boolean
-    onClick: any
+    setSelected: (value: 1 | 2 | 3 | 4 | 5 ) => void
+    value: 1 | 2 | 3 | 4 | 5
 }
 
 // component
@@ -19,34 +19,23 @@ function Rating(props: RatingProps) {
 
     let [selected, setSelected] = useState(2)
 
-    const setSelectStar = () => {
-
-    }
-
     return (
         <div className='Rating'>
             <h3 className='ratingTitle'>Rating:</h3>
-            <Star id={1} selected={ selected > 0} onClick={ () => { setSelected(1) }} />
-            <Star id={2} selected={ selected > 1} onClick={ () => { setSelected(2) }} />
-            <Star id={3} selected={ selected > 2} onClick={ () => { setSelected(3) }} />
-            <Star id={4} selected={ selected > 3} onClick={ () => { setSelected(4) }} />
-            <Star id={5} selected={ selected > 4} onClick={ () => { setSelected(5) }} />
+            <Star selected={selected > 0} setSelected={setSelected} value={1} />
+            <Star selected={selected > 1} setSelected={setSelected} value={2} />
+            <Star selected={selected > 2} setSelected={setSelected} value={3} />
+            <Star selected={selected > 3} setSelected={setSelected} value={4} />
+            <Star selected={selected > 4} setSelected={setSelected} value={5} />
         </div>
     )
 }
 
 function Star(props: StarPropsType) {
-
-
-    if (!props.selected) {
-        return <button key={props.id}
-                       onClick={props.onClick}
-                       className='ButtonStar'> {starImg} </button>
-    } else {
-        return <button key={props.id}
-                       onClick={props.onClick}
-                       className='ButtonStarCheck'> {starImg} </button>
-    }
+    return <button className={!props.selected ? 'ButtonStar' : 'ButtonStarCheck'}
+                   onClick={() => {
+                       props.setSelected(props.value)
+                   }}> {starImg} </button>
 }
 
 export default Rating;
