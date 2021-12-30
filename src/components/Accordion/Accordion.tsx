f5741d53608550129456cf8854c5d29ac0c8357f
Marinade <marinadegames@gmail.com>
2021-12-30 20:14:41 +0300

    added Storybook | fix storybook

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,28 +1,28 @@
 //imports
 import React from "react";
+import s from './Accordion.module.css'
 
-//type
+//types
 type AccordionPropsTitle = {
     title: string
-
-
 }
+
 type AccordionPropsCollapsed = {
     collapsed?: boolean,
 }
 
 function Accordion(props: AccordionPropsTitle & AccordionPropsCollapsed){
     return(
-        <div className='Accordion'>
+        <div className={s.accordion}>
             <AccordionTitle title={props.title}/>
             { !props.collapsed && <AccordionBody/> }
         </div>
     )
 }
+
 function AccordionTitle(props: AccordionPropsTitle){
     return <h3>{props.title}</h3>
 }
-
 function AccordionBody(props:any){
 
     if (props.collapsed === true){
